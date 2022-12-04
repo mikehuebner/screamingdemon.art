ff91ce13a2bc71b00370a365fda8843a7ac57197
mikehuebner <mikehuebner@me.com>
2022-12-04 13:01:16 -0800

    feat: add sanity

---
+++ apps/sanity/sanity.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { schemaTypes } from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Screaming Demon Art',
+
+  projectId: '3ipunxqe',
+  dataset: 'production',
+
+  plugins: [deskTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
