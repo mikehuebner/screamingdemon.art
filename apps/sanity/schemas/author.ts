dca003b916718e9b12cf527c0368e1d3ccbc49cf
mikehuebner <mikehuebner@me.com>
2022-12-04 17:34:59 -0800

    chore: trigger a deploy

--- apps/sanity/schemas/author.ts
+++ apps/sanity/schemas/author.ts
@@ -6,6 +6,11 @@ export default defineType({
   type: 'document',
   fields: [
     defineField({
+      name: 'test',
+      title: 'Test',
+      type: 'string',
+    }),
+    defineField({
       name: 'name',
       title: 'Name',
       type: 'string',
